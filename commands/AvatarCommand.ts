f4c0f5276233bbe1ca3207c2f9e42d99a2c772a5
geixco <68980781+geixco@users.noreply.github.com>
2021-05-21 23:49:44 -0400

    sex

--- commands/AvatarCommand.ts
+++ commands/AvatarCommand.ts
@@ -5,13 +5,11 @@ import { Result, TrollCommand } from '../TrollCommand';
 export const AvatarCommand = new TrollCommand(client, {
   name: 'avatar',
   description: 'string',
-  aliases: ['string[]'],
-  usage: 'string',
+  usage: 'avatar:troll: [User]',
   nsfw: false,
   arguments: [{ name: 'User', type: 'USER' }],
   run: async (message: Message, args: [User], flags: Map<string, string>): Promise<Result | undefined> => {
-    message.channel.send(args[0].displayAvatarURL());
-
-    return undefined;
+    message.channel.send((args[0] || message.author).displayAvatarURL());
+    return { code: 'OK' }
   },
 });
