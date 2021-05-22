71038dedf8304feff37a99e017e34df513f0bd0f
geixco <68980781+geixco@users.noreply.github.com>
2021-05-21 23:00:29 -0400

    Update TestCommand.ts

--- commands/TestCommand.ts
+++ commands/TestCommand.ts
@@ -24,30 +24,3 @@ export const TestCommand = new TrollCommand(client, {
     }
   },
 });
-
-/*export class TestCommand extends TrollCommand {
-  constructor(client: TrollClient) {
-    super(client, {
-      names: ['test', 'alias'],
-      description: 'emj',
-      argCount: undefined,
-      usage: '',
-      permissions: {
-        client: ['MANAGE_MESSAGES'],
-        user: ['MANAGE_EMOJIS']
-      },
-      accessibility: {
-        owner: true
-      },
-      run(message: Message): Result | undefined {
-        try {
-        message.channel.send('<:water:689488678380503041>')
-        } catch (error) {
-          return { code: 'ERROR', error: error }
-        } finally {
-          return { code: 'INFO', details: '${user} ran command "${command.constructir}"' }
-        }
-      },
-    });
-  }
-}*/
