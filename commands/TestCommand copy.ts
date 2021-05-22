e78959dd6b65a25934154547e6a50ca9ac944891
geixco <68980781+geixco@users.noreply.github.com>
2021-05-21 23:27:59 -0400

    bigsex

---
+++ commands/TestCommand copy.ts
@@ -0,0 +1,17 @@
+import { Message, User } from 'discord.js';
+import { client } from '../TrollClient';
+import { Result, TrollCommand } from '../TrollCommand';
+
+export const AvatarCommand = new TrollCommand(client, {
+  name: 'avatar',
+  description: 'string',
+  aliases: ['string[]'],
+  usage: 'string',
+  nsfw: false,
+  arguments: [{ name: 'User', type: 'USER' }],
+  run: async (message: Message, args: [User], flags: Map<string, string>): Promise<Result | undefined> => {
+    console.log(args);
+
+    return undefined;
+  },
+});
