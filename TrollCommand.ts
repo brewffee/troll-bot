b4b0d8429fe0cf623bb66b7fd6c5d51aa69c695d
ToastOnAStick <matt.zhao.18@gmail.com>
2021-05-21 21:36:17 -0400

    feat(cmd): Argument parsing, argument flags, and formatting code
    Arguments are now parsed into structures before being passed to commands. Arguments now contain optional flags.

--- TrollCommand.ts
+++ TrollCommand.ts
@@ -1,50 +1,69 @@
-import { Message, PermissionResolvable } from "discord.js";
+import { DiscordAPIError, GuildChannel, GuildMember, Message, PermissionResolvable, Role, User } from 'discord.js';
 import { TrollClient } from './TrollClient';
 
 interface CommandOptions {
-  names: string[],
-  description: string,
-  argCount?: [number, string],
-  usage: string,
-  nsfw?: boolean,
-  permissions?: {
-    client?: PermissionResolvable[],
-    user?: PermissionResolvable[],
-  }
-  accessibility?: {
-    guildOnly?: boolean
-    owner?: boolean,
-    admins?: boolean,
-    mods?: boolean,
-  },
-  run: Function,
+    names: string[];
+    description: string;
+    argCount?: [number, string];
+    usage: string;
+    nsfw?: boolean;
+    permissions?: {
+        client?: PermissionResolvable[];
+        user?: PermissionResolvable[];
+    };
+    accessibility?: {
+        guildOnly?: boolean;
+        owner?: boolean;
+        admins?: boolean;
+        mods?: boolean;
+    };
+    arguments?: Argument[];
+    run: (message: Message, args: ArgumentType[], flags: Map<string, string>) => Promise<Result | undefined>;
 }
 
 export class TrollCommand {
-  public info: CommandOptions;
-  public isAuthorized: Function;
-  public run: Function;
-  constructor(client: TrollClient, info: CommandOptions) {
-    Object.defineProperty(this, 'client', { value: client, enumerable: false })
-    this.info = info;
-    this.run = info.run;
-    this.isAuthorized = ({ member, guild }: Message): boolean => {
-      let authorized: boolean = false;
-      if (!this.info.accessibility) {
-        authorized = true;
-      } else if (this.info.accessibility.owner) {
-        authorized = member!.id === guild!.ownerID
-      } else if (this.info.accessibility.admins) {
-        authorized = member!.roles.cache.some(r => r.id === '842802299675213824') || member!.id === guild!.ownerID
-      } else if (this.info.accessibility.admins) {
-        authorized = member!.roles.cache.some(r => ['842802299675213824', '842802567499219005'].includes(r.id)) || member!.id === guild!.ownerID
-      } 
-      
-      if (authorized || this.info.permissions) {
-        authorized = member!.permissions.has(this.info.permissions!.user!, true)
-      }
+    public info: CommandOptions;
+    public arguments: Argument[];
+    public isAuthorized: Function;
+    public run: Function;
+    constructor(client: TrollClient, info: CommandOptions) {
+        Object.defineProperty(this, 'client', { value: client, enumerable: false });
+        this.info = info;
+        this.run = info.run;
+        this.arguments = info.arguments?.map((argument) => ({ required: true, ...argument })) ?? [];
+        this.isAuthorized = ({ member, guild }: Message): boolean => {
+            let authorized: boolean = false;
+            if (!this.info.accessibility) {
+                authorized = true;
+            } else if (this.info.accessibility.owner) {
+                authorized = member!.id === guild!.ownerID;
+            } else if (this.info.accessibility.admins) {
+                authorized = member!.roles.cache.some((r) => r.id === '842802299675213824') || member!.id === guild!.ownerID;
+            } else if (this.info.accessibility.admins) {
+                authorized = member!.roles.cache.some((r) => ['842802299675213824', '842802567499219005'].includes(r.id)) || member!.id === guild!.ownerID;
+            }
 
-      return authorized
+            if (authorized || this.info.permissions) {
+                authorized = member!.permissions.has(this.info.permissions!.user!, true);
+            }
+
+            return authorized;
+        };
     }
-  }
 }
+
+export interface Result {
+    code: string;
+    details?: string;
+    error?: Error | DiscordAPIError;
+}
+
+export interface Argument {
+    name: string;
+    required?: boolean;
+    type: 'STRING' | 'NUMBER' | 'MEMBER' | 'USER' | 'CHANNEL' | 'ROLE';
+}
+
+export interface Flag {}
+
+export type ArgumentType = string | number | null | undefined | GuildMember | User | GuildChannel | Role;
--- commands/TestCommand.ts
+++ commands/TestCommand.ts
@@ -1,26 +1,53 @@
-import { Message } from "discord.js";
-import { client } from "../TrollClient";
-import { TrollCommand } from "../TrollCommand";
+import { Message } from 'discord.js';
+import { TrollClient, client } from '../TrollClient';
+import { TrollCommand, Result } from '../TrollCommand';
 
 export const TestCommand = new TrollCommand(client, {
-  names: ['test', 'alias'],
-  description: 'Test command',
-  argCount: undefined,
-  usage: '',
-  // permissions: {
-  //  client: ['MANAGE_MESSAGES'],
-  //  user: ['MANAGE_EMOJIS']
-  // },
-  // accessibility: {
-  //   owner: true
-  // },
-  run(message: Message) {
-    try {
-      message.channel.send('<:water:689488678380503041>')
-    } catch (error) {
-      return { code: 'ERROR', error: error }
-    } finally {
-      return { code: 'INFO', details: '${user} ran command "${command.constructir}"' }
-    }
+    names: ['test', 'alias'],
+    description: 'emj',
+    argCount: undefined,
+    usage: '',
+    permissions: {
+        client: ['MANAGE_MESSAGES'],
+        user: ['MANAGE_EMOJIS'],
+    },
+    accessibility: {
+        owner: true,
+    },
+    async run(message, args, flags) {
+        try {
+            message.channel.send('<:water:689488678380503041>');
+        } catch (error) {
+            return { code: 'ERROR', error: error };
+        } finally {
+            return { code: 'INFO', details: '${user} ran command "${command.constructir}"' };
+        }
+    },
+});
+
+/*export class TestCommand extends TrollCommand {
+  constructor(client: TrollClient) {
+    super(client, {
+      names: ['test', 'alias'],
+      description: 'emj',
+      argCount: undefined,
+      usage: '',
+      permissions: {
+        client: ['MANAGE_MESSAGES'],
+        user: ['MANAGE_EMOJIS']
+      },
+      accessibility: {
+        owner: true
+      },
+      run(message: Message): Result | undefined {
+        try {
+        message.channel.send('<:water:689488678380503041>')
+        } catch (error) {
+          return { code: 'ERROR', error: error }
+        } finally {
+          return { code: 'INFO', details: '${user} ran command "${command.constructir}"' }
+        }
+      },
+    });
   }
-})
+}*/
