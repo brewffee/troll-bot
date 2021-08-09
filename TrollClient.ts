23ef17331b466783fb1c1b7c4f9bbd875b4d406d
geixco <68980781+geixco@users.noreply.github.com>
2021-08-09 19:38:14 -0400

    make config a separate file

--- TrollClient.ts
+++ TrollClient.ts
@@ -2,15 +2,16 @@ import { BufferResolvable, ChannelResolvable, Client, Collection, EmojiResolvabl
 import { readdir } from 'fs';
 import { TrollCommand } from './TrollCommand';
 import { TrollEvent } from './TrollEvent';
+import { config } from './config';
 
-interface TrollConfig {
+export interface TrollConfig {
   troll: EmojiResolvable;
   suffix: string;
   cake: EmojiResolvable;
   reddit: EmojiResolvable[]; // enable feature disabling later :P
   iconChannel: ChannelResolvable;
   general: Snowflake;
-  responses: Array<[RegExp, string, BufferResolvable | null]>;
+  responses: Array<[RegExp, string, BufferResolvable?]>;
   botRole: RoleResolvable;
   memberRole: RoleResolvable;
   adminRole: RoleResolvable;
@@ -46,28 +47,4 @@ export class TrollClient extends Client {
 
 export const client = new TrollClient();
 
-client.load({ // create a separate config.js for this later
-  troll: '<:troll:841760436042203138>',
-  suffix: '<:troll:841760436042203138>',
-  cake: '<:cakeday:874408986936492082>',
-  reddit: [
-    '<:rsilver:843164309735735316>',
-    '<:rgold:843160855234215968>',
-    '<:rplat:843164215786340442>',
-    '<:wholesome:843164215346069546>'
-  ],
-  iconChannel: '841159137781874698',
-  general: '840829257004875789',
-  responses: [
-    // if anyone wants to rewrite these regexes PLEASE PR holy shit they're bad
-    [/\b(y((o+u'?r+e?)|(o+|e|a))( are)? m((o+ther+)|(o+|u)m)(m+y+)?)/gi, 'i am doing your mother', './images/mother.png'],
-    [/\bbu+s{2,}y+/gi, 'hnng <:cum:841142405846925312>'],
-    [/\bto+p+/gi, '*bottom'],
-    [/🥺|https:\/\/discord\.com\/assets\/6bca769662f755d33514d1f5304c617d\.svg/gi, 'what the fuck is "🥺" i dont speak bottom'],
-    [/\b((amo+n?g+)|s+u+(s+|ß+)|impost|vent|pretender|cre+wma+te|medbay|electrical)/gi, 'SUSSY!', './images/amogsus.png']
-  ],
-  botRole: '841267799787438090',
-  memberRole: '841295461486428200',
-  adminRole: '842802299675213824',
-  modRole: '842802567499219005',
-});
+client.load(config);
