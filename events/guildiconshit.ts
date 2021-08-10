a96213e63c63c15258a27e247cc7bd3b47818e98
geixco <68980781+geixco@users.noreply.github.com>
2021-08-09 22:33:49 -0400

    change of terminology

--- events/guildiconshit.ts
+++ events/guildiconshit.ts
@@ -13,7 +13,7 @@ export const guildIconShit = new TrollEvent(client, {
     const url = message.attachments?.first()?.url || message.content;
     if (/(https?:\/\/.+\.(jpe?g|png|webp|gif)(\?.+)?)/i.test(url)) {
       let size: number = await (await fetch(url, {method: 'HEAD'})).headers.get('content-length') / 1000
-      if (size > 10240) return message.channel.send('bro you expect me to upload all of that? upload something less than 10mb goddamn');
+      if (size > 10240) return message.channel.send('bro you expect me to upload all of that? send something less than 10mb goddamn');
       else if (size * 1000 < 300) return message.channel.send('i need a microscope to see that shit 😐');
       return Math.floor(Math.random() * 2) === 1
         ? message.channel.send('changed the guild icon').then(() => message.guild!.setIcon(url))
