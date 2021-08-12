792e38ea425ef78fd42d1c62ce8d780ffd5520df
geixco <68980781+geixco@users.noreply.github.com>
2021-08-12 12:13:50 -0400

    allow searching for other users' xp

--- commands/XPCommand.ts
+++ commands/XPCommand.ts
@@ -1,3 +1,4 @@
+import { Message, User } from 'discord.js';
 import { client } from '../TrollClient';
 import { TrollCommand } from '../TrollCommand';
 import { getStats, xp } from '../xp';
@@ -5,15 +6,22 @@ import { getStats, xp } from '../xp';
 export const TestCommand = new TrollCommand(client, {
   name: 'xp',
   aliases: ['experience', 'levels', 'karma'],
-  description: 'check out how much xp ya got',  
-  async run(message) {
+  description: 'check out how much xp ya got',
+  arguments: [{ name: 'User', type: 'USER', required: false }],
+  async run(message: Message, args: [User]) {
     try {
       // allows for getting xp on first msg
-      await xp.findOne({ id: message.author.id });
+      await xp.findOne({ id: (args[0] || message.author).id });
 
-      const stats = await getStats(message.author.id);
+      const stats = await getStats((args[0] || message.author).id);
       const awards = ['<:rplat:843164215786340442>', '<:rgold:843160855234215968>', '<:rsilver:843164309735735316>'];
-      message.channel.send(`you've got **${stats.xp}** karma, placing you at ${stats.place < 4 ? awards[stats.place - 1] + ' ' : ''}**#${stats.place}** in trolling ${client.config.troll}`)
+      message.channel.send(
+        (args[0]
+          ? `${args[0].username.toLowerCase()} has **${stats.xp}** karma, placing them at`
+          : `you've got **${stats.xp}** karma, placing you at`
+        ) + ` ${stats.place < 4 ? awards[stats.place - 1] + ' ' : ''}**#${stats.place}** in trolling ${client.config.troll}`
+      );
+
     } catch (error) {
       return { code: 'ERROR', error: error };
     } finally {
