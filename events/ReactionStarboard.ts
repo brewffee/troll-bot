f262b3b7001314579d7d6b1a7e83a57215ec711e
SirH <micahbenac52@gmail.com>
2022-05-01 18:33:12 -0400

    add incremental stars on starboard

--- events/ReactionStarboard.ts
+++ events/ReactionStarboard.ts
@@ -3,28 +3,36 @@ import { client, type TrollClient } from '../TrollClient';
 import { TrollEvent } from '../TrollEvent';
 import { starboard } from '../models/Starboard';
 
+const STAR_REQUIREMENT = 3;
+
 export const ReactionStarboard = new TrollEvent(client, {
   name: 'ReactionStarboard',
   description: 'add messages to a starboard if it reaches a certain amount of star reactions',
   type: 'messageReactionAdd',
   run: async (client: TrollClient, reaction: MessageReaction, user: User) => {
     const reactionCount = reaction.users.cache.has(reaction.message.author.id) ? reaction.count - 1 : reaction.count;
-    if (reaction.emoji.name != '⭐' || reactionCount != 1 || reaction.message.channel.id == '970366685771079810') return;
-    const starboardMessage = await starboard.findOne({ message_id: reaction.message.id });
+    if (reaction.emoji.name != '⭐' || reactionCount < 1 || reaction.message.channel.id == '970366685771079810') return;
+    const starboardMessageData = await starboard.findOne({ message_id: reaction.message.id });
 
-    if (starboardMessage) return; // already in the starboard
-    
-    const starboardChannel = client.channels.cache.get('970366685771079810') as TextChannel;
-   
     // define shit to make it look nicer lul
+    const starboardChannel = client.channels.cache.get('970366685771079810') as TextChannel;
     const starCount = `**${reactionCount}**⭐ `;
     const channelName = `${reaction.message.channel as TextChannel} `;
     const displayName = reaction.message.member.nickname ? `**${reaction.message.member.nickname}** (${reaction.message.author.tag})` : `**${reaction.message.author.tag}**`;
+
+    if (starboardMessageData) { // already in the starboard
+      if (reactionCount <= starboardMessageData.star_count) return;
+      const starboardMessage = await starboardChannel.messages.fetch(starboardMessageData.starboard_message_id);
+
+      return starboardMessage.edit({ content: starCount + channelName + displayName + '\n' + starboardMessageData.content })
+        .then(async () => await starboard.findOneAndUpdate({ message_id: reaction.message.id }, { star_count: reactionCount }));
+    }
+
     const messageContent = reaction.message.content.length > 1024 ? reaction.message.content.substring(0, 1021) + '...' : reaction.message.content;
     const files = reaction.message.attachments.size > 0 ? Array.from(reaction.message.attachments.values()) : undefined;
 
     // constuct everything, send message and the files
     return starboardChannel.send({ content: starCount + channelName + displayName + '\n' + messageContent, files })
-      .then(async () => await starboard.create({ message_id: reaction.message.id }));
+      .then(async (message) => await starboard.create({ message_id: reaction.message.id, starboard_message_id: message.id, content: messageContent, star_count: STAR_REQUIREMENT }));
   },
 });
--- models/Starboard.ts
+++ models/Starboard.ts
@@ -3,10 +3,16 @@ import mongoose from 'mongoose';
 
 export interface StarboardFormat extends mongoose.Document {
   message_id: Snowflake,
+  starboard_message_id: Snowflake,
+  content: string,
+  star_count: number,
 }
 
 const StarboardSchema = new mongoose.Schema({
   message_id: String,
+  starboard_message_id: String,
+  content: String,
+  star_count: Number,
 });
 
 export const starboard = mongoose.model<StarboardFormat>("starboard", StarboardSchema, "starboard");
