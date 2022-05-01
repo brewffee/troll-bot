d11659fdfef738d667c473579db71ad0aedd2ef4
mochaa ♡ <68980781+mxchakitty@users.noreply.github.com>
2022-05-01 18:14:29 -0400

    switch to regular message format

--- events/ReactionStarboard.ts
+++ events/ReactionStarboard.ts
@@ -1,4 +1,4 @@
-import { MessageEmbed, MessageReaction, TextChannel, User } from 'discord.js';
+import { MessageReaction, TextChannel, User } from 'discord.js';
 import { client, type TrollClient } from '../TrollClient';
 import { TrollEvent } from '../TrollEvent';
 import { starboard } from '../models/Starboard';
@@ -9,26 +9,22 @@ export const ReactionStarboard = new TrollEvent(client, {
   type: 'messageReactionAdd',
   run: async (client: TrollClient, reaction: MessageReaction, user: User) => {
     const reactionCount = reaction.users.cache.has(reaction.message.author.id) ? reaction.count - 1 : reaction.count;
-    if (reaction.emoji.name != '⭐' || reactionCount != 3 || reaction.message.channel.id == '970366685771079810') return;
+    if (reaction.emoji.name != '⭐' || reactionCount != 1 || reaction.message.channel.id == '970366685771079810') return;
     const starboardMessage = await starboard.findOne({ message_id: reaction.message.id });
 
     if (starboardMessage) return; // already in the starboard
     
     const starboardChannel = client.channels.cache.get('970366685771079810') as TextChannel;
-    let description = reaction.message.content.length > 1024 ? reaction.message.content.substring(0, 1021) + '...' : reaction.message.content;
-    if (reaction.message.stickers.size > 0) description += `\n*contains sticker*`; 
-    const embed = new MessageEmbed()
-      .setAuthor({ name: reaction.message.author.tag, iconURL: reaction.message.author.displayAvatarURL() })
-      .setDescription(`${description}\n\n[Jump to message](${reaction.message.url})`)
-      .setColor('#ffd700')
-      .setFooter({ text: `#${(reaction.message.channel as TextChannel).name}` })
-      .setTimestamp(reaction.message.createdTimestamp);
+   
+    // define shit to make it look nicer lul
+    const starCount = `**${reactionCount}**⭐ `;
+    const channelName = `${reaction.message.channel as TextChannel} `;
+    const displayName = reaction.message.member.nickname ? `**${reaction.message.member.nickname}** (${reaction.message.author.tag})` : `**${reaction.message.author.tag}**`;
+    const messageContent = reaction.message.content.length > 1024 ? reaction.message.content.substring(0, 1021) + '...' : reaction.message.content;
+    const files = reaction.message.attachments.size > 0 ? Array.from(reaction.message.attachments.values()) : undefined;
 
-    const firstAttachment = reaction.message.attachments.first();
-    if (firstAttachment?.proxyURL.endsWith('.png') || firstAttachment?.proxyURL.endsWith('.gif')) embed.setImage(reaction.message.attachments.first()!.proxyURL);
-    else if (firstAttachment?.proxyURL.endsWith('.mp4')) embed.setDescription(`${embed.description}\n\n*contains video*`);
-
-    return starboardChannel.send({ embeds: [embed] })
+    // constuct everything, send message and the files
+    return starboardChannel.send({ content: starCount + channelName + displayName + '\n' + messageContent, files })
       .then(async () => await starboard.create({ message_id: reaction.message.id }));
   },
 });
